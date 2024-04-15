f02ddcce5c81f284dd92ab786b0d051aca71fb0c
dk10119 <tranduclong092@gmail.com>
2024-04-15 13:12:22 +0300

    fetch animal data from js file and display with card

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
 import Card from "./components/home/Card";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
+import { animals, birds, fishes, insects } from "./assets/animalsList";
 import './App.css';
 
 function App() {
-
   return (
     <>
     <div className="wrapper shadow-xl bg-white">
       <Header />
 
       <div className="main p-10 grid grid-cols-4 gap-5">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {animals.map(animal => <Card key={animal.name} {...animal}/>)}
       </div>
 
       <Footer />
