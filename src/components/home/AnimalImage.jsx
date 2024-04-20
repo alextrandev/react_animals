1a53bb883b5d5945b703db56427f3e82b412e864
dk10119 <tranduclong092@gmail.com>
2024-04-20 16:55:49 +0300

    create a component for animal image

---
+++ src/components/home/AnimalImage.jsx
@@ -0,0 +1,5 @@
+export default function AnimalImage({name}) {
+  return (
+    <img src="/react.svg" className="w-full p-5" alt={`image of a/an ${name}`}/>
+  )
+}
--- src/components/home/Card.jsx
+++ src/components/home/Card.jsx
@@ -1,7 +1,9 @@
+import AnimalImage from "./AnimalImage";
+
 function Card({name}) {
   return (
     <div className="bg-gray-200 rounded shadow overflow-hidden">
-        <img src="/react.svg" className="w-full p-5" alt="animal image"/>
+        <AnimalImage name={name}/>
         <div className="text-center bg-gray-600 p-4">
             <h3 className="text-xl text-white">{name}</h3>
         </div>
