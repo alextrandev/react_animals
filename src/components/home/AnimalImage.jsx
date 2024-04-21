575037b3a66a711d0391d9cbd302dd00fe5a6de4
dk10119 <tranduclong092@gmail.com>
2024-04-21 21:49:10 +0300

    create a pexels query based on animal name an category

--- src/components/home/AnimalImage.jsx
+++ src/components/home/AnimalImage.jsx
@@ -1,19 +1,34 @@
-import axios from 'axios';
 import { useState } from 'react';
+import { createClient } from 'pexels';
 
-export default function AnimalImage({name}) {
-  // console.log(import.meta.env.VITE_API_KEY);
+export default function AnimalImage({name, categoryName}) {
   const [imageLink, setImageLink] = useState("");
-  const imageQuery = `https://api.iconify.design/search?query=${name}&limit=32`;
-  axios
-    .get(imageQuery)
-    .then(response => {
-      const imageName = response.data.icons[0] ?? "cil:animal";
-      const imageLinkParts = imageName.split(":");
-      setImageLink(`https://api.iconify.design/${imageLinkParts[0]}/${imageLinkParts[1]}.svg`);
-    })
-    .catch(error => console.log(error))
-    return (
-      <img src={imageLink} className="w-full p-5" alt={`image of a/an ${name}`}/>
-    )
+
+  let categoryQuery = "animal";
+  
+  switch (categoryName) {
+    case "Mammals":
+      categoryQuery = "animal";
+      break;
+    case "Birds":
+      categoryQuery = "bird";
+      break;
+    case "Fishes":
+      categoryQuery = "fish";
+      break;
+    case "Insects":
+      categoryQuery = "insect";
+      break;
+  }
+
+  const query = name + " " + categoryQuery;
+  const client = createClient(import.meta.env.VITE_API_KEY);
+
+  client.photos.search({ query,  orientation: "landscape", per_page: 1 }).then(photos => {
+    setImageLink(photos.photos[0].src.original);
+  });
+
+  return (
+    <img src={imageLink} className="h-full" alt={`image of a/an ${name}`}/>
+  )
 }
