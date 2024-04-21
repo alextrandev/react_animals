6c77db6f285b3acf41792ab288c7380d59942ad1
dk10119 <tranduclong092@gmail.com>
2024-04-21 10:42:22 +0300

    create pagination buttons and page content logic

--- src/components/home/AnimalsGrid.jsx
+++ src/components/home/AnimalsGrid.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import Card from "./Card";
 
 export default function AnimalsGrid({animals, categoryName}) {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const animalsPerPage = 21; // 7 item per row, render 3 rows
+  const numberOfAnimals = animals.length;
+  const numberOfPages = Math.ceil(numberOfAnimals / animalsPerPage);
+
+  // console.log(animalsNumber);
 
   return (
     <>
         <h2 className="text-xl font-bold p-10 pb-1">{categoryName}</h2>
         <div className="main p-10 pt-1 grid grid-cols-7 gap-2">
             {animals.map(animal => <Card key={animal.name} {...animal}/>)}
+        </div>
+        <div>
+          <input type="button" name="previous" value="< Previous" />
+          <input type="button" name="next" value="Next >" />
         </div>
     </>
   )
