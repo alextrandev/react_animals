9b5420bbecf54408736115ec16fc2b8ef148b3e9
dk10119 <tranduclong092@gmail.com>
2024-04-22 12:26:19 +0300

    fetch likes from local json file, add some stylings

--- src/components/home/Card.jsx
+++ src/components/home/Card.jsx
@@ -1,7 +1,7 @@
 import AnimalImage from "./AnimalImage";
 import LikeCounter from "./LikeCounter";
 
-function Card({name, categoryName}) {
+function Card({name, likes, categoryName}) {
   const formatedName = name.charAt(0).toUpperCase() + name.slice(1).replace("-", " ");
   return (
     <div className="bg-gray-200 flex flex-col justify-between rounded shadow overflow-hidden">
@@ -9,7 +9,7 @@ function Card({name, categoryName}) {
             <h3 className="text-xs text-white">{formatedName}</h3>
         </div>
         <AnimalImage name={name} categoryName={categoryName}/>
-        <LikeCounter />
+        <LikeCounter likes={likes}/>
     </div>
   )
 }
--- src/components/home/LikeCounter.jsx
+++ src/components/home/LikeCounter.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
-export default function LikeCounter() {
-  const [likeCounter, setLikeCounter] = useState(0);
+export default function LikeCounter({likes}) {
+  const [likeCounter, setLikeCounter] = useState(likes);
   
   const handleLike = () => {
     setLikeCounter(likeCounter + 1);
@@ -11,7 +11,6 @@ export default function LikeCounter() {
     setLikeCounter(likeCounter -1);
   }
 
-
   return (
     <div className="flex justify-between p-2">
         <span 
@@ -20,13 +19,16 @@ export default function LikeCounter() {
         >thumb_up</span>
         <div className="flex gap-1">
             <p>{likeCounter}</p>
-            <span
-                className="material-symbols-rounded text-red-500"
-                onClick={handleDislike}
-            >favorite</span>
-            {/* <span className="material-symbols-rounded">heart_broken</span> */}
+            {likeCounter <= -5 && <span className="material-symbols-rounded">sentiment_very_dissatisfied</span>}
+            {likeCounter < 0 && likeCounter > -5 && <span className="material-symbols-rounded text-red-500">mood_bad</span>}
+            {likeCounter == 0 && <span className="material-symbols-rounded text-blue-500">sentiment_satisfied</span>}
+            {likeCounter > 0 && likeCounter < 5 &&<span className="material-symbols-rounded text-green-500">mood</span>}
+            {likeCounter >= 5 && <span className="material-symbols-rounded text-green-500">sentiment_very_satisfied</span>}
         </div>
-        <span className="material-symbols-rounded text-red-500">thumb_down</span>
+        <span 
+            className="material-symbols-rounded text-red-500"
+            onClick={handleDislike}
+        >thumb_down</span>
     </div>
   )
 }
