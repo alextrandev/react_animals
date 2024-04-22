6636139f670d23fed1933f09a811563bbfeb73c4
dk10119 <tranduclong092@gmail.com>
2024-04-22 09:44:59 +0300

    trying to fix pexel bugs when deploy the site

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+window.require = (name) => new URL(name, import.meta.url).href;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
