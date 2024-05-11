c7b94787827962c623bbda99d7f4e0b51ec82206
dk10119 <tranduclong092@gmail.com>
2024-05-11 19:01:14 +0300

    Refactor: move App component to it own file to complice with eslint rule allowing fast refresh

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './components/layout/Root';
+import Home from './pages/Home.jsx';
+import { animals, birds, fishes, insects } from './assets/animalsList.js';
+import CategoryPage from './pages/CategoryPage.jsx';
+import AnimalPage from './pages/AnimalPage.jsx';
+
+export default function App() {
+  const [zoo, setZoo] = useState({ animals, birds, fishes, insects })
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: ":category", element: <CategoryPage {...zoo} /> },
+        { path: ":category/:animal", element: <AnimalPage {...zoo} /> }
+      ]
+    }
+  ])
+  return <RouterProvider router={router} />
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import './main.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './components/layout/Root';
-import Home from './pages/Home.jsx';
-import { animals, birds, fishes, insects } from './assets/animalsList.js';
-import CategoryPage from './pages/CategoryPage.jsx';
-import AnimalPage from './pages/AnimalPage.jsx';
+import App from './App';
 window.require = (name) => new URL(name, import.meta.url).href;
 console.log("Copyright © Alex Tran (https://alextran.dev)");
-
-function App() {
-  const [zoo, setZoo] = useState({ animals, birds, fishes, insects })
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      children: [
-        { path: "/", element: <Home /> },
-        { path: ":category", element: <CategoryPage {...zoo} /> },
-        { path: ":category/:animal", element: <AnimalPage {...zoo} /> }
-      ]
-    }
-  ])
-  return <RouterProvider router={router} />
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
